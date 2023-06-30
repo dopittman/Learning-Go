c2e2f9fd1ae4936691cc10bd1cf4e5d0f3838b66
David Pittman <53283971+dopittman@users.noreply.github.com>
2023-06-30 19:51:40 -0400

    feat: add additional messages

--- Greetings/greetings.go
+++ Greetings/greetings.go
@@ -1,10 +1,29 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("Hello: empty name")
+	}
+
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
+}
+
+func Goodbye(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("Goodbye: empty name")
+	}
+
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Goodbye, %v!", name)
+	return message, nil
 }
