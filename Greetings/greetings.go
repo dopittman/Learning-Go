6c52d886df1a06558fed8754c6790d87da957e5a
David Pittman <53283971+dopittman@users.noreply.github.com>
2023-06-30 19:57:38 -0400

    feat: randomise greetings and goodbyes

--- Greetings/greetings.go
+++ Greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
@@ -13,7 +14,7 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomHelloFormat(), name)
 	return message, nil
 }
 
@@ -24,6 +25,36 @@ func Goodbye(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Goodbye, %v!", name)
+	message := fmt.Sprintf(randomGoodbyeFormat(), name)
 	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomHelloFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomGoodbyeFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Later, %v.",
+		"Adios, %v!",
+		"Hasta la vista, %v!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
