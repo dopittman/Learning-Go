f3b8c371993eb651a23d64c181e8d9ccce7dc491
David Pittman <53283971+dopittman@users.noreply.github.com>
2023-07-01 16:43:51 -0400

    feat: add support for multiple greetings

--- Hello/hello.go
+++ Hello/hello.go
@@ -14,6 +14,21 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	for k := range messages {
+		fmt.Printf("%s\n", messages[k])
+	}
+
 	// Request a greeting message.
 	message, helloErr := greetings.Hello("David")
 	goodbye, goodbyeErr := greetings.Goodbye("David")
